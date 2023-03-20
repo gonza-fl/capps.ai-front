6b9c0c7b0069f753b98e71c239530dd629e06c50
UpmarketBat23 <75809408+UpmarketBat23@users.noreply.github.com>
2023-03-20 18:01:12 -0300

    *+ ADD*: Se creo el FrontEnd RegisterScreen y LoginScreen (#5)
    * *+ ADD*: Se creo el FrontEnd RegisterScreen y LoginScreen
    
    * Update App.tsx
    
    Se cambiaron las double quotes a single quotes
    
    * Rename AccoutText.tsx to AccountText.tsx
    
    Se corrigio el nombre del archivo
    
    * Update LoginScreen.tsx
    
    Se cambio el nombre del archivo de AccoutText a AccountText
    
    * Update RegisterScreen.tsx
    
    Se cambio el nombre del archivo de AccoutText a AccountText
    
    * Update App.tsx
    
    * Update AccountText.tsx
    
    Se cambiaron las double quotes a single quotes
    
    * Update ButtonCApps.tsx
    
    Se cambiaron las double quotes a single quotes
    
    * Update InputForm.tsx
    
    Se cambiaron las double quotes a single quotes
    
    * Update TermsAndConditions.tsx
    
    Se cambiaron las double quotes a single quotes
    
    * Update LoginScreen.tsx
    
    Se cambiaron las double quotes a single quotes
    
    * Update RegisterScreen.tsx
    
    Se cambiaron las double quotes a single quotes

--- App.tsx
+++ App.tsx
@@ -1,15 +1,58 @@
-import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Register from './src/screens/RegisterScreen';
+import Login from './src/screens/LoginScreen';
+import { ParamListBase } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
-export default function App() {
+interface Props {
+  navigation: NativeStackNavigationProp<ParamListBase>;
+}
+
+function HomeScreen({ navigation }: Props): JSX.Element {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
+      <Button
+        title='Go to Register'
+        onPress={() => navigation.navigate("Register")}
+      />
+      <Button
+        title='Go to Login'
+        onPress={() => navigation.navigate("Login")}
+      />
     </View>
   );
 }
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName='Home'
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen
+          name='Home'
+          component={HomeScreen}
+          options={{ title: 'Overview' }}
+        />
+        <Stack.Screen name='Register' component={Register} />
+        <Stack.Screen name='Login' component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
---
+++ src/screens/RegisterScreen.tsx
@@ -0,0 +1,56 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import InputForm from '../components/InputForm';
+import ButtonCAps from '../components/ButtonCApps';
+import TermsAndConditions from '../components/TermsAndConditions';
+import AccountText from '../components/AccountText';
+// import CheckBox from '@react-native-community/checkbox';
+
+export default function Register() {
+  // const [value, setValue] = useState(false);
+
+  return (
+    <View style={styles.container}>
+      <StatusBar style='auto' backgroundColor='#999' />
+      <View style={(styles.title, styles.margin)}>
+        <Text style={[styles.titleText]}>Bienvenidos a CApps</Text>
+        <InputForm
+          Content={'nombre completo'}
+          Placeholder={'arnaldo benito camelo mclovin'}
+        />
+        <InputForm Content={'email'} Placeholder={'benito@camelo.com'} />
+        <InputForm Content={'contraseña'} Placeholder={'su contraseña'} />
+        <InputForm
+          Content={'confirmar contraseña'}
+          Placeholder={'repita su contraseña'}
+        />
+        <TermsAndConditions />
+        {/* <CheckBox value={value} onValueChange={(val) => setValue(val)} /> */}
+        <ButtonCAps Title='Registrarme' />
+        <AccountText Title='Ya tengo una cuenta' />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  title: {
+    flex: 1,
+    alignItems: 'baseline',
+    justifyContent: 'flex-start',
+  },
+  margin: {
+    marginHorizontal: 30,
+  },
+  titleText: {
+    fontSize: 23,
+    fontWeight: '500',
+    marginTop: 110,
+    marginBottom: 20,
+  },
+});
